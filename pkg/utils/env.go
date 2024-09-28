b9ed788c90c6651940e9803dea7c6025ec70f306
haryoiro <mizusecocolte@gmail.com>
2024-09-28 18:27:25 +0900

    🚑 RUNをCMDに

--- pkg/utils/env.go
+++ pkg/utils/env.go
@@ -1,15 +1,12 @@
 package utils
 
 import (
-	"fmt"
-
-	"braces.dev/errtrace"
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() error {
 	if err := godotenv.Load(".env"); err != nil {
-		return errtrace.Wrap(fmt.Errorf("failed to load .env file: %w", err))
+		return nil
 	}
 	return nil
 }
