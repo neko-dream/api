f4c49cedd5d64b40198d8d2a91939d6fd36c3945
haryoiro <mizusecocolte@gmail.com>
2024-10-01 18:41:44 +0900

    👍 RegisterUserハンドラを実装

--- internal/presentation/handler/user_handler.go
+++ internal/presentation/handler/user_handler.go
@@ -2,13 +2,19 @@ package handler
 
 import (
 	"context"
-	"log"
 
+	"github.com/neko-dream/server/internal/domain/messages"
 	"github.com/neko-dream/server/internal/domain/model/session"
+	"github.com/neko-dream/server/internal/infrastructure/db"
 	"github.com/neko-dream/server/internal/presentation/oas"
+	user_usecase "github.com/neko-dream/server/internal/usecase/user"
+	"github.com/neko-dream/server/pkg/utils"
+	"github.com/samber/lo"
 )
 
 type userHandler struct {
+	*db.DBManager
+	user_usecase.RegisterUserUseCase
 }
 
 // EditUserProfile implements oas.UserHandler.
@@ -18,21 +24,69 @@ func (u *userHandler) EditUserProfile(ctx context.Context) (*oas.EditUserProfile
 
 // GetUserProfile implements oas.UserHandler.
 func (u *userHandler) GetUserProfile(ctx context.Context) (*oas.GetUserProfileOK, error) {
-	panic("unimplemented")
-}
 
-// RegisterUser implements oas.UserHandler.
-func (u *userHandler) RegisterUser(ctx context.Context, params oas.RegisterUserParams) (oas.RegisterUserRes, error) {
-	claim := session.GetSession(ctx)
-	log.Println(claim)
-
-	return &oas.RegisterUserOK{
+	return &oas.GetUserProfileOK{
 		DisplayID:   "string",
 		DisplayName: "string",
 	}, nil
-
 }
 
-func NewUserHandler() oas.UserHandler {
-	return &userHandler{}
+// RegisterUser ユーザー登録
+func (u *userHandler) RegisterUser(ctx context.Context, params oas.OptRegisterUserReq) (oas.RegisterUserRes, error) {
+	claim := session.GetSession(ctx)
+	if !params.IsSet() {
+		return nil, messages.RequiredParameterError
+	}
+	value := params.Value
+	if err := value.Validate(); err != nil {
+		utils.HandleError(ctx, err, "value.Validate")
+		return nil, messages.RequiredParameterError
+	}
+	userID, err := claim.UserID()
+	if err != nil {
+		utils.HandleError(ctx, err, "claim.UserID")
+		return nil, messages.InternalServerError
+	}
+	genderByte, err := value.Gender.MarshalText()
+	if err != nil {
+		utils.HandleError(ctx, err, "value.Gender")
+		return nil, messages.InternalServerError
+	}
+	gender := string(genderByte)
+	occupationByte, err := value.Occupation.Value.MarshalText()
+	if err != nil {
+		utils.HandleError(ctx, err, "value.Occupation")
+		return nil, messages.InternalServerError
+	}
+	occupation := string(occupationByte)
+
+	input := user_usecase.RegisterUserInput{
+		UserID:        userID,
+		DisplayID:     value.DisplayID,
+		DisplayName:   value.DisplayName,
+		PictureURL:    claim.Picture,
+		YearOfBirth:   &value.YearOfBirth.Value,
+		Gender:        lo.ToPtr(gender),
+		Municipality:  lo.ToPtr(value.Municipality.Value),
+		Occupation:    lo.ToPtr(occupation),
+		HouseholdSize: &value.HouseholdSize.Value,
+	}
+	out, err := u.RegisterUserUseCase.Execute(ctx, input)
+	if err != nil {
+		utils.HandleError(ctx, err, "RegisterUserUseCase.Execute")
+		return nil, messages.InternalServerError
+	}
+
+	return &oas.RegisterUserOK{
+		DisplayID:   out.DisplayID,
+		DisplayName: out.DisplayName,
+	}, nil
+}
+
+func NewUserHandler(
+	DBManager *db.DBManager,
+) oas.UserHandler {
+	return &userHandler{
+		DBManager: DBManager,
+	}
 }
