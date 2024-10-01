1add9f630f9404a1d08d658641f5ede3e793b56e
haryoiro <mizusecocolte@gmail.com>
2024-10-01 22:00:42 +0900

    👍 関数の場合と単純な値の場合で関数を分けた

--- pkg/utils/ptr.go
+++ pkg/utils/ptr.go
@@ -1,8 +1,16 @@
 package utils
 
-func ToPtrIfNotNull[T any](nullFlag bool, value T) *T {
+func ToPtrIfNotNullValue[T any](nullFlag bool, value T) *T {
 	if nullFlag {
 		return nil
 	}
 	return &value
+}
+
+func ToPtrIfNotNullFunc[T any](nullFlag bool, getValue func() T) *T {
+	if nullFlag {
+		return nil
+	}
+	val := getValue()
+	return &val
 }
