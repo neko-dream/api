99dc90367744fd47c51a4bad07f698fc1ca5602f
haryoiro <mizusecocolte@gmail.com>
2024-10-01 18:39:07 +0900

    👍 ユーザ関連のエラー定義

---
+++ internal/domain/messages/user.go
@@ -0,0 +1,35 @@
+package messages
+
+var (
+	UserNotFoundError = &APIError{
+		StatusCode: 404,
+		Code:       "USER-0000",
+		Message:    "ユーザが見つかりません。再度ログインしてください。",
+	}
+
+	UserDisplayIDAlreadyExistsError = &APIError{
+		StatusCode: 400,
+		Code:       "USER-0001",
+		Message:    "そのIDは既に使用されています。",
+	}
+	UserDisplayIDInvalidError = &APIError{
+		StatusCode: 400,
+		Code:       "USER-0002",
+		Message:    "IDは半角英数字で入力してください。",
+	}
+	UserDisplayIDTooLong = &APIError{
+		StatusCode: 400,
+		Code:       "USER-0003",
+		Message:    "IDは30文字以内で入力してください。",
+	}
+	UserDisplayIDTooShort = &APIError{
+		StatusCode: 400,
+		Code:       "USER-0004",
+		Message:    "IDは4文字以上で入力してください。",
+	}
+	UserUpdateError = &APIError{
+		StatusCode: 500,
+		Code:       "USER-0005",
+		Message:    "ユーザ情報の更新に失敗しました。",
+	}
+)
