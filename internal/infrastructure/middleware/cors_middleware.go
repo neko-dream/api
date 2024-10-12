fd04a83f2182aeea4f52205d586c4708670661f4
haryoiro <mizusecocolte@gmail.com>
2024-10-12 20:13:51 +0900

    🚑 AllowCredentialsいれた

--- internal/infrastructure/middleware/cors_middleware.go
+++ internal/infrastructure/middleware/cors_middleware.go
@@ -6,9 +6,10 @@ import (
 
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "127.0.0.1:60487, localhost:3000, api.kotohiro.com, kotohiro.com, local.kotohiro.com:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "127.0.0.1:60487,localhost:3000,api.kotohiro.com,kotohiro.com,local.kotohiro.com:3000")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
