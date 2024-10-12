72a14d28ccd64cc2f5ae4ca2db396dc5eb4e7212
haryoiro <mizusecocolte@gmail.com>
2024-10-12 21:18:48 +0900

    👍 全てのURLを許可するように

--- internal/infrastructure/middleware/cors_middleware.go
+++ internal/infrastructure/middleware/cors_middleware.go
@@ -6,7 +6,7 @@ import (
 
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "127.0.0.1:60487,localhost:3000,api.kotohiro.com,kotohiro.com,http://local.kotohiro.com:3000,https://local.kotohiro.com:3000,local.kotohiro.com:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "http://*,https://*, localhost")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
