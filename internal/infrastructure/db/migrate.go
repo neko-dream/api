119d82318f147039919182231f0addcf46e6f695
haryoiro <mizusecocolte@gmail.com>
2024-10-12 17:59:59 +0900

    👍 デバッグログ

--- internal/infrastructure/db/migrate.go
+++ internal/infrastructure/db/migrate.go
@@ -22,10 +22,12 @@ func NewMigrator(config *config.Config) *Migrator {
 func (m *Migrator) Up() {
 	pgx, err := sql.Open("pgx", m.config.DatabaseURL)
 	if err != nil {
+		log.Println("Panic in pgx")
 		panic(err)
 	}
 	driver, err := postgres.WithInstance(pgx, &postgres.Config{})
 	if err != nil {
+		log.Println("Panic in postgres")
 		panic(err)
 	}
 	mi, err := migrate.NewWithDatabaseInstance(
@@ -34,6 +36,7 @@ func (m *Migrator) Up() {
 		driver,
 	)
 	if err != nil {
+		log.Println("Panic in migrate")
 		panic(err)
 	}
 	if err := mi.Up(); err != nil {
@@ -46,11 +49,15 @@ func (m *Migrator) Up() {
 func (m *Migrator) Down() {
 	pgx, err := sql.Open("pgx", m.config.DatabaseURL)
 	if err != nil {
+		log.Println("Panic in pgx")
 		panic(err)
 	}
-	pgx.Exec("SET session_replication_role = replica;")
+	if _, err := pgx.Exec("SET session_replication_role = replica;"); err != nil {
+		log.Println("Error setting session replication role:", err)
+	}
 	driver, err := postgres.WithInstance(pgx, &postgres.Config{})
 	if err != nil {
+		log.Println("Panic in postgres")
 		panic(err)
 	}
 	mi, err := migrate.NewWithDatabaseInstance(
