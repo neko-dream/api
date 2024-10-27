606f6147254781b0d3b4097648c9bbedeffb00c2
haryoiro <mizusecocolte@gmail.com>
2024-10-27 21:18:21 +0900

    ✨ フローとコメントのテーブルちか

---
+++ migrations/00011_add_talk_session_result_table.down.sql
@@ -0,0 +1,6 @@
+DROP INDEX IF EXISTS idx_talk_session_conclusions_talk_session_id;
+DROP INDEX IF EXISTS idx_talk_session_conclusions_creator;
+
+DROP TABLE IF EXISTS action_items;
+DROP TABLE IF EXISTS talk_session_conclusions;
+
