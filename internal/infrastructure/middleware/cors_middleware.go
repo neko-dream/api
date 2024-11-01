9e7f5c155983af917c03ece835f7391cc3cb48bc
haryoiro <mizusecocolte@gmail.com>
2024-11-01 21:54:49 +0900

    👍 デバッグログ

--- internal/infrastructure/middleware/cors_middleware.go
+++ internal/infrastructure/middleware/cors_middleware.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"log"
 	"net/http"
 )
 
@@ -11,7 +10,6 @@ func CORSMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		log.Println("header Cookie", r.Header.Get("Cookie"))
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
--- internal/infrastructure/middleware/req_middleware.go
+++ internal/infrastructure/middleware/req_middleware.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"log"
 	"net/http"
 
 	http_utils "github.com/neko-dream/server/pkg/http"
@@ -9,7 +10,8 @@ import (
 func ReqMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		r = r.WithContext(http_utils.WithHTTPResReqContext(r.Context(), r, w))
-
+		log.Println("Request URL: ", r.URL)
+		log.Println("COOKIE HEADER: ", r.Header.Get("Cookie"))
 		next.ServeHTTP(w, r)
 	})
 }
