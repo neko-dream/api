6fe74f9fcf39a632fadb6799d6c1db18cabb7822
haryoiro <mizusecocolte@gmail.com>
2024-11-09 12:09:51 +0900

    👍 現在時刻をctxで指定できるclock.Now(ctx)を定義

---
+++ internal/domain/model/clock/clock.go
@@ -0,0 +1,24 @@
+package clock
+
+import (
+	"context"
+	"time"
+)
+
+var (
+	// timeKey 現在時刻を取得するためのContextKey
+	timeKey = &struct{}{}
+)
+
+// Now 現在時刻を取得する
+func Now(ctx context.Context) time.Time {
+	if t, ok := ctx.Value(timeKey).(time.Time); ok {
+		return t
+	}
+	return time.Now()
+}
+
+// SetNow 現在時刻を設定する
+func SetNow(ctx context.Context, t time.Time) context.Context {
+	return context.WithValue(ctx, timeKey, t)
+}
