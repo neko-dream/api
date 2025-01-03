038e0328e183970e1007bc6713ef2ba429574ed9
haryoiro <mizusecocolte@gmail.com>
2025-01-03 13:02:52 +0900

    ♻️ DBManagerをpersistenceに移動

--- internal/infrastructure/persistence/transaction/manager.go
+++ internal/infrastructure/persistence/transaction/manager.go
@@ -3,26 +3,41 @@ package db
 import (
 	"context"
 	"database/sql"
+	"fmt"
 
 	"braces.dev/errtrace"
-	model "github.com/neko-dream/server/internal/infrastructure/db/sqlc"
+	model "github.com/neko-dream/server/internal/persistance/transaction/sqlc"
 	"github.com/neko-dream/server/pkg/utils"
 )
+
+type txKey struct{}
+
+func getTx(ctx context.Context) *sql.Tx {
+	if tx, ok := ctx.Value(txKey{}).(*sql.Tx); ok {
+		return tx
+	}
+	return nil
+}
 
 // DBManager DBコネクションおよびトランザクションを管理
 type DBManager struct {
 	db *sql.DB
 }
 
-func (s *DBManager) TestTx(ctx context.Context, fn func(ctx context.Context) error) error {
+func NewDBManager(db *sql.DB) *DBManager {
+	return &DBManager{
+		db: db,
+	}
+}
 
+func (s *DBManager) TestTx(ctx context.Context, fn func(ctx context.Context) error) error {
 	tx, err := s.db.BeginTx(ctx, nil)
 	if err != nil {
 		utils.HandleError(ctx, err, "トランザクションの開始に失敗")
 		return errtrace.Wrap(err)
 	}
 
-	if err := fn(context.WithValue(ctx, key, tx)); err != nil {
+	if err := fn(context.WithValue(ctx, txKey{}, tx)); err != nil {
 		utils.HandleError(ctx, err, "トランザクション内の処理で失敗")
 		return err
 	}
@@ -36,32 +51,34 @@ func (s *DBManager) TestTx(ctx context.Context, fn func(ctx context.Context) err
 }
 
 func (s *DBManager) ExecTx(ctx context.Context, fn func(context.Context) error) error {
-	panicked := true
 	var tx *sql.Tx
 
-	if tmpTx := getTransaction(ctx); tmpTx != nil {
+	if tmpTx := getTx(ctx); tmpTx != nil {
 		// トランザクションが既に開始されているのならそれを使う
-		tx = tmpTx
-	} else {
-		// トランザクションが開始されていない場合は新しくトランザクションを開始する
-		tmpTx, err := s.db.BeginTx(ctx, nil)
-		if err != nil {
-			utils.HandleError(ctx, err, "トランザクションの開始に失敗")
-			return errtrace.Wrap(err)
-		}
-		tx = tmpTx
+		return fn(ctx)
+	}
+
+	// トランザクションが開始されていない場合は新しくトランザクションを開始する
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		utils.HandleError(ctx, err, "トランザクションの開始に失敗")
+		return errtrace.Wrap(err)
 	}
 
 	defer func() {
-		if panicked {
+		if r := recover(); r != nil {
 			if rbErr := tx.Rollback(); rbErr != nil {
 				utils.HandleError(ctx, rbErr, "トランザクションのロールバックに失敗")
 			}
 		}
 	}()
 
-	if err := fn(context.WithValue(ctx, key, tx)); err != nil {
-		utils.HandleError(ctx, err, "トランザクション内の処理で失敗")
+	txCtx := context.WithValue(ctx, txKey{}, tx)
+	if err := fn(txCtx); err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			utils.HandleError(ctx, rbErr, fmt.Sprintf("ロールバックに失敗: %v (元エラー: %v)", rbErr, err))
+			return fmt.Errorf("ロールバックに失敗: %v (元エラー: %w)", rbErr, err)
+		}
 		return err
 	}
 
@@ -70,37 +87,16 @@ func (s *DBManager) ExecTx(ctx context.Context, fn func(context.Context) error) 
 		return errtrace.Wrap(err)
 	}
 
-	panicked = false
-
 	return nil
 }
 
 // GetQueries トランザクションが開始されている場合はトラトランザクションを返す。そうでない場合はDBコネクションを返す。
 func (s *DBManager) GetQueries(ctx context.Context) *model.Queries {
 	// トランザクションが開始されている場合はトランザクションを返す
-	if tx, ok := ctx.Value(key).(*sql.Tx); ok {
+	if tx := getTx(ctx); tx != nil {
 		return model.New(tx)
 	}
+
 	// トランザクションが開始されていない場合はDBコネクションを返す
 	return model.New(s.db)
-}
-
-type transactionCtxKey string
-
-const (
-	key transactionCtxKey = "TransactionKey"
-)
-
-func getTransaction(ctx context.Context) *sql.Tx {
-	tx, ok := ctx.Value(key).(*sql.Tx)
-	if ok && tx != nil {
-		return tx
-	}
-	return nil
-}
-
-func NewDBManager(db *sql.DB) *DBManager {
-	return &DBManager{
-		db: db,
-	}
 }
