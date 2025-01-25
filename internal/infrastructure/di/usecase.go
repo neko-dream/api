ca9dc2f68041e4457f48ea8f774b23847dec5b86
haryoiro <mizusecocolte@gmail.com>
2025-01-25 16:50:09 +0900

    🚿 authを移し、いらんものを削除

--- internal/infrastructure/di/usecase.go
+++ internal/infrastructure/di/usecase.go
@@ -6,7 +6,6 @@ import (
 	user_query "github.com/neko-dream/server/internal/infrastructure/persistence/query/user"
 
 	analysis_usecase "github.com/neko-dream/server/internal/usecase/analysis"
-	auth_usecase "github.com/neko-dream/server/internal/usecase/auth"
 	"github.com/neko-dream/server/internal/usecase/command/auth_command"
 	"github.com/neko-dream/server/internal/usecase/command/opinion_command"
 	"github.com/neko-dream/server/internal/usecase/command/talksession_command"
@@ -17,9 +16,6 @@ import (
 
 func useCaseDeps() []ProvideArg {
 	return []ProvideArg{
-		{auth_usecase.NewAuthLoginUseCase, nil},
-		{auth_usecase.NewAuthCallbackUseCase, nil},
-		{auth_usecase.NewRevokeUseCase, nil},
 		{analysis_usecase.NewGetAnalysisResultUseCase, nil},
 		{analysis_usecase.NewGetReportQueryHandler, nil},
 		{timeline_usecase.NewAddTimeLineUseCase, nil},
