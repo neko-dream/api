33d308deb6ba1a65d61ecf918e2b2a1d11852920
haryoiro <mizusecocolte@gmail.com>
2025-02-16 16:26:35 +0900

    👍 画像のバリデーションなども含めて画像ドメインで行えるように

--- internal/domain/model/image/image.go
+++ internal/domain/model/image/image.go
@@ -1,103 +1,47 @@
 package image
 
 import (
-	"bytes"
 	"context"
-	"image"
 
 	"io"
-	"mime/multipart"
-	"strings"
 
-	"github.com/neko-dream/server/internal/domain/messages"
-
-	_ "image/jpeg"
-	_ "image/png"
-
-	"github.com/h2non/filetype"
-	"github.com/h2non/filetype/types"
+	"github.com/neko-dream/server/internal/domain/model/image/meta"
+	"github.com/neko-dream/server/internal/domain/model/shared"
+	"github.com/neko-dream/server/internal/domain/model/user"
 	"go.opentelemetry.io/otel"
 )
 
 type (
-	Image []byte
+	ImageStorage interface {
+		Upload(context.Context, meta.ImageMeta, io.Reader) (*string, error)
+	}
+	ImageRepository interface {
+		Save(context.Context, *UserImage) error
+		FindByID(context.Context, shared.UUID[UserImage]) (*UserImage, error)
+		FindByUserID(context.Context, shared.UUID[user.User]) ([]*UserImage, error)
+	}
+
+	UserImage struct {
+		UserImageID shared.UUID[UserImage]
+		UserID      shared.UUID[user.User]
+		Metadata    meta.ImageMeta
+	}
 )
 
-func NewImage(image []byte) Image {
-	return Image(image)
-}
-
-func (i *Image) GetImage() []byte {
-	return *i
-}
-func (i *Image) GetImageReader() io.Reader {
-	return bytes.NewReader(*i)
-}
-
-func CheckSize(imageByte []byte, x, y int) error {
-	// 画像のサイズを取得
-	img, _, err := image.Decode(bytes.NewReader(imageByte))
-	if err != nil {
-		return messages.ImageDecodeFailedError
-	}
-
-	// 画像のサイズが指定したサイズより大きい場合はエラー
-	if img.Bounds().Dx() > x || img.Bounds().Dy() > y {
-		return messages.ImageSizeTooLargeError
-	}
-
-	return nil
-}
-
-var supportedExtension = "png jpg"
-
-// ValidateImage 拡張子やファイルサイズなどの画像のバリデーションを行う
-func ValidateImage(ctx context.Context, file *multipart.FileHeader, maxSize int) ([]byte, *string, error) {
-	ctx, span := otel.Tracer("image").Start(ctx, "ValidateImage")
+func NewUserImage(
+	ctx context.Context,
+	userImageID shared.UUID[UserImage],
+	userID shared.UUID[user.User],
+	metadata meta.ImageMeta,
+) *UserImage {
+	ctx, span := otel.Tracer("image").Start(ctx, "NewUserImage")
 	defer span.End()
 
 	_ = ctx
 
-	img, err := file.Open()
-	if err != nil {
-		return nil, nil, messages.ImageOpenFailedError
+	return &UserImage{
+		UserImageID: userImageID,
+		UserID:      userID,
+		Metadata:    metadata,
 	}
-
-	buf := new(bytes.Buffer)
-	if _, err := buf.ReadFrom(img); err != nil {
-		return nil, nil, messages.ImageOpenFailedError
-	}
-
-	// 最大サイズに収まっているか
-	if buf.Len() >= maxSize {
-		return nil, nil, messages.ImageSizeTooLargeError
-	}
-
-	// 対応画像形式か
-	reader, t, err := detectExt(buf)
-	// ファイル形式を検知するのに必要なバイト数だけ先に読む
-	if err != nil || !strings.Contains(supportedExtension, t.Extension) {
-		return nil, nil, messages.ImageUnsupportedExtError
-	}
-	body, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, nil, messages.ImageOpenFailedError
-	}
-
-	return body, &t.MIME.Value, nil
-}
-
-func detectExt(file *bytes.Buffer) (io.Reader, types.Type, error) {
-	header := bytes.NewBuffer(nil)
-	reader := io.TeeReader(file, header)
-
-	head := make([]byte, 261)
-	if _, err := reader.Read(head); err != nil {
-		return nil, types.Unknown, err
-	}
-	t, e := filetype.Match(head)
-
-	multiReader := io.MultiReader(header, file)
-	return multiReader, t, e
-
 }
---
+++ internal/domain/model/image/meta/meta.go
@@ -0,0 +1,169 @@
+package meta
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+
+	"github.com/h2non/filetype/types"
+	"github.com/neko-dream/server/internal/domain/model/clock"
+	"github.com/neko-dream/server/internal/domain/model/opinion"
+	"github.com/neko-dream/server/internal/domain/model/shared"
+	"github.com/neko-dream/server/internal/domain/model/user"
+	"go.opentelemetry.io/otel"
+)
+
+type ImageMeta struct {
+	Key       string
+	Size      int
+	Extension types.MIME
+	width     int
+	height    int
+	Archived  bool
+}
+
+var (
+	ProfileImageKeyPattern = "users/%s.jpg"
+	// ReferenceImageKeyPattern Year/Month/Day/opinionID.jpg
+	ReferenceImageKeyPattern = "reference_images/%v/%v/%v/%v.jpg"
+	// 種類-talkSessionID-時間.jpg
+	AnalysisImageKeyPattern = "generated/%v-%v-%v.png"
+)
+
+// ProfileIcon用の画像メタデータを生成
+func NewImageForProfile(
+	ctx context.Context,
+	userID shared.UUID[user.User],
+	file io.Reader,
+) (*ImageMeta, error) {
+	ctx, span := otel.Tracer("meta").Start(ctx, "NewImageForProfile")
+	defer span.End()
+
+	key := fmt.Sprintf(ProfileImageKeyPattern, userID.String())
+
+	size, err := GetImageSize(ctx, file)
+	if err != nil {
+		return nil, err
+	}
+	ext, err := GetExtension(ctx, file)
+	if err != nil {
+		return nil, err
+	}
+
+	x, y, err := GetBounds(ctx, file)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ImageMeta{
+		Key:       key,
+		Size:      size,
+		Extension: ext,
+		width:     x,
+		height:    y,
+	}, nil
+}
+
+// ReferenceImage用の画像メタデータを生成
+func NewImageForReference(
+	ctx context.Context,
+	opinionID shared.UUID[opinion.Opinion],
+	file io.Reader,
+) (*ImageMeta, error) {
+	ctx, span := otel.Tracer("meta").Start(ctx, "NewImageForReference")
+	defer span.End()
+
+	now := clock.Now(ctx)
+	key := fmt.Sprintf(
+		ReferenceImageKeyPattern,
+		now.Year(),
+		now.Month(),
+		now.Day(),
+		opinionID.String(),
+	)
+
+	size, err := GetImageSize(ctx, file)
+	if err != nil {
+		return nil, err
+	}
+	ext, err := GetExtension(ctx, file)
+	if err != nil {
+		return nil, err
+	}
+
+	x, y, err := GetBounds(ctx, file)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ImageMeta{
+		Key:       key,
+		Size:      size,
+		Extension: ext,
+		width:     x,
+		height:    y,
+	}, nil
+}
+
+func NewImageForAnalysis(
+	ctx context.Context,
+	file io.Reader,
+) (*ImageMeta, error) {
+	ctx, span := otel.Tracer("meta").Start(ctx, "NewImageForAnalysis")
+	defer span.End()
+
+	now := clock.Now(ctx)
+	key := fmt.Sprintf(
+		AnalysisImageKeyPattern,
+		"analysis",
+		now.Format("20060102150405"),
+	)
+
+	size, err := GetImageSize(ctx, file)
+	if err != nil {
+		return nil, err
+	}
+	ext, err := GetExtension(ctx, file)
+	if err != nil {
+		return nil, err
+	}
+
+	x, y, err := GetBounds(ctx, file)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ImageMeta{
+		Key:       key,
+		Size:      size,
+		Extension: ext,
+		width:     x,
+		height:    y,
+	}, nil
+}
+
+func (m *ImageMeta) Validate(ctx context.Context, rule ImageValidationRule) error {
+	ctx, span := otel.Tracer("meta").Start(ctx, "ImageMeta.Validate")
+	defer span.End()
+
+	var err error
+
+	if !rule.ValidExtension(ctx, *m) {
+		err = errors.Join(err, errors.New("サポートされていないフォーマットです。"))
+	}
+
+	if !rule.ValidFileSize(ctx, *m) {
+		err = errors.Join(err, errors.New("ファイルサイズが大きすぎます。"))
+	}
+
+	if !rule.ValidBounds(ctx, *m) {
+		err = errors.Join(err, errors.New("画像のサイズが大きすぎます。"))
+	}
+
+	if !rule.ValidAspectRatio(ctx, *m) {
+		err = errors.Join(err, errors.New("アスペクト比が不正です。"))
+	}
+
+	return err
+}
