f91db4c6ff9de113e0efb8ceecbe7723c8b99d06
haryoiro <mizusecocolte@gmail.com>
2025-02-25 21:56:33 +0900

    👍 sqlcで生成されたクエリ

--- internal/infrastructure/persistence/sqlc/generated/models.go
+++ internal/infrastructure/persistence/sqlc/generated/models.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/sqlc-dev/pqtype"
 )
 
 type ActionItem struct {
@@ -65,6 +66,8 @@ type TalkSession struct {
 	Prefecture       sql.NullString
 	Description      sql.NullString
 	ThumbnailUrl     sql.NullString
+	Restrictions     pqtype.NullRawMessage
+	UpdatedAt        time.Time
 }
 
 type TalkSessionConclusion struct {
