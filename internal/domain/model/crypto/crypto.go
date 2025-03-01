c8f5b59e3adbb85e284dbda4a164673d2816b182
haryoiro <mizusecocolte@gmail.com>
2025-03-01 17:20:14 +0900

    🚿 encryptedValueはいらないため修正

--- internal/domain/model/crypto/crypto.go
+++ internal/domain/model/crypto/crypto.go
@@ -8,15 +8,3 @@ type Encryptor interface {
 	EncryptInt(plaintext int64) (string, error)
 	DecryptInt(ciphertext string) (int64, error)
 }
-
-type EncryptedValue struct {
-	value string
-}
-
-func NewEncryptedValue(value string) EncryptedValue {
-	return EncryptedValue{value: value}
-}
-
-func (e EncryptedValue) Value() string {
-	return e.value
-}
