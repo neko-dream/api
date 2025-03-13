08172f666a3387b64d8c37f153bcb20799f07d01
haryoiro <mizusecocolte@gmail.com>
2025-03-13 22:10:51 +0900

    👍 GetOpinionDetail2を定義

--- internal/presentation/oas/oas_response_encoders_gen.go
+++ internal/presentation/oas/oas_response_encoders_gen.go
@@ -529,6 +529,39 @@ func encodeGetOpinionDetailResponse(response GetOpinionDetailRes, w http.Respons
 	}
 }
 
+func encodeGetOpinionDetail2Response(response GetOpinionDetail2Res, w http.ResponseWriter, span trace.Span) error {
+	switch response := response.(type) {
+	case *GetOpinionDetail2OK:
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(200)
+		span.SetStatus(codes.Ok, http.StatusText(200))
+
+		e := new(jx.Encoder)
+		response.Encode(e)
+		if _, err := e.WriteTo(w); err != nil {
+			return errors.Wrap(err, "write")
+		}
+
+		return nil
+
+	case *GetOpinionDetail2InternalServerError:
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(500)
+		span.SetStatus(codes.Error, http.StatusText(500))
+
+		e := new(jx.Encoder)
+		response.Encode(e)
+		if _, err := e.WriteTo(w); err != nil {
+			return errors.Wrap(err, "write")
+		}
+
+		return nil
+
+	default:
+		return errors.Errorf("unexpected response type: %T", response)
+	}
+}
+
 func encodeGetOpinionsForTalkSessionResponse(response GetOpinionsForTalkSessionRes, w http.ResponseWriter, span trace.Span) error {
 	switch response := response.(type) {
 	case *GetOpinionsForTalkSessionOK:
