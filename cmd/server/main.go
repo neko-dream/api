ae07f97ade3772d15fb8d6380cb6aea1f9092f83
haryoiro <mizusecocolte@gmail.com>
2025-03-15 16:28:52 +0900

    👍 CORSに対応

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -39,7 +39,7 @@ func main() {
 	}
 
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"https://*.kotohiro.com", "http://localhost:*"},
+		AllowedOrigins:   []string{"https://*.kotohiro.com", "http://localhost:*", "https://*.kotohiro.com:*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"*"},
 		AllowCredentials: true,
