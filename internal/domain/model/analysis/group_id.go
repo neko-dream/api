073276f44558b2374aef0dfe373ab16d07b9e930
haryoiro <mizusecocolte@gmail.com>
2025-03-25 22:47:23 +0900

    🚑 GroupNameを果物からアルファベットに

--- internal/domain/model/analysis/group_id.go
+++ internal/domain/model/analysis/group_id.go
@@ -12,26 +12,30 @@ const (
 
 var (
 	GroupIDNameMap = map[GroupID]string{
-		0: "いちご",
-		1: "レモン",
-		2: "ぶどう",
-		3: "すいか",
+		0:  "A",
+		1:  "B",
+		2:  "C",
+		3:  "D",
+		4:  "E",
+		5:  "F",
+		6:  "G",
+		7:  "H",
+		8:  "I",
+		9:  "J",
+		10: "K",
+		11: "L",
+		12: "M",
+		13: "N",
+		14: "O",
+		15: "P",
+		16: "Q",
+		17: "R",
+		18: "S",
 	}
 )
 
 func NewGroupIDFromInt(i int) GroupID {
-	switch i {
-	case 0:
-		return GroupIDStrawberry
-	case 1:
-		return GroupIDLemon
-	case 2:
-		return GroupIDGrape
-	case 3:
-		return GroupIDWatermelon
-	default:
-		return GroupIDUnknown
-	}
+	return GroupID(i)
 }
 
 func (g GroupID) String() string {
