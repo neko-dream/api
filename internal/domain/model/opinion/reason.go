278132012fad9891b668cd680b7fd1209d37e346
haryoiro <mizusecocolte@gmail.com>
2025-03-25 21:01:16 +0900

    👍 理由を日本語にできるように

--- internal/domain/model/opinion/reason.go
+++ internal/domain/model/opinion/reason.go
@@ -2,21 +2,32 @@ package opinion
 
 type Reason int32
 
+//go:generate go tool enumer -type=Reason -trimprefix=Reason -transform=kebab -linecomment
 const (
-	// ReasonInappropriate 不適切な内容
+	// 不適切な内容
 	ReasonInappropriate Reason = iota + 1
-	// ReasonSpam スパム
+	// セッションとは関係のない発言
+	ReasonIrrelevant
+	// スパム
 	ReasonSpam
-	// ReasonHarassment 嫌がらせ
-	ReasonHarassment
-	// ReasonOther その他
-	ReasonOther
+	// プライバシー
+	ReasonPrivacy
+	// その他
+	ReasonOther = 255
 )
 
-func NewReason(reason int32) Reason {
-	// validation
-	if reason < 1 || reason > 4 {
-		return ReasonOther
+// StringJP
+func (r Reason) StringJP() string {
+	switch r {
+	case ReasonInappropriate:
+		return "不適切な内容"
+	case ReasonIrrelevant:
+		return "セッションとは関係のない発言"
+	case ReasonSpam:
+		return "スパム"
+	case ReasonPrivacy:
+		return "プライバシー"
+	default:
+		return "その他"
 	}
-	return Reason(reason)
 }
