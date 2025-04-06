5b5d9ddccab5748ef400eac15e1a81655b06a0ed
haryoiro <mizusecocolte@gmail.com>
2025-04-06 20:45:24 +0900

    ✨ 通報を解決するAPIを追加

--- internal/infrastructure/di/usecase.go
+++ internal/infrastructure/di/usecase.go
@@ -11,6 +11,7 @@ import (
 	"github.com/neko-dream/server/internal/usecase/command/image_command"
 	"github.com/neko-dream/server/internal/usecase/command/opinion_command"
 	"github.com/neko-dream/server/internal/usecase/command/policy_command"
+	"github.com/neko-dream/server/internal/usecase/command/report_command"
 	"github.com/neko-dream/server/internal/usecase/command/talksession_command"
 	"github.com/neko-dream/server/internal/usecase/command/timeline_command"
 	"github.com/neko-dream/server/internal/usecase/command/user_command"
@@ -61,5 +62,6 @@ func useCaseDeps() []ProvideArg {
 		{talksession_command.NewEditCommand, nil},
 		{report_query.NewGetByTalkSessionQueryInteractor, nil},
 		{report_query.NewGetOpinionReportQueryInteractor, nil},
+		{report_command.NewSolveReportCommandInteractor, nil},
 	}
 }
