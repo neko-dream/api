2e3ed62d0b8282a5fd145349828ae1ef64925a0d
haryoiro <mizusecocolte@gmail.com>
2025-04-13 19:07:07 +0900

    🚑 パスワード生成

--- internal/domain/model/auth/password/generator.go
+++ internal/domain/model/auth/password/generator.go
@@ -1,13 +1,28 @@
 package password_auth
 
 import (
-	"github.com/sethvargo/go-password/password"
+	"crypto/rand"
+	"math/big"
 )
 
-func GeneratePassword(length int) (string, error) {
-	pass, err := password.Generate(length, 10, 10, true, false)
-	if err != nil {
-		return "", err
+func GeneratePassword(length int) string {
+	var letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	var specialBytes = "!@#$%^&*()_+-=[]{}|;:,.<>?"
+	var numBytes = "0123456789"
+
+	var allBytes string
+
+	allBytes += letterBytes
+
+	allBytes += specialBytes
+
+	allBytes += numBytes
+
+	b := make([]byte, length)
+	for i := range b {
+		n, _ := rand.Int(rand.Reader, big.NewInt(int64(len(allBytes))))
+		b[i] = allBytes[n.Int64()]
 	}
-	return pass, nil
+
+	return string(b)
 }
