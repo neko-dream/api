f491a5547b424a60dfc98ab52e28e3c6d5b87621
haryoiro <mizusecocolte@gmail.com>
2025-04-18 21:08:52 +0900

    👍 エラーメッセージ追加

--- internal/domain/messages/organization.go
+++ internal/domain/messages/organization.go
@@ -13,4 +13,19 @@ var (
 		Code:       "ORGANIZATION-002",
 		Message:    "この操作は許可されていません",
 	}
+	UserAlreadyInOrganization = &APIError{
+		StatusCode: http.StatusBadRequest,
+		Code:       "ORGANIZATION-003",
+		Message:    "ユーザーはすでに組織に参加しています",
+	}
+	OrganizationNotFound = &APIError{
+		StatusCode: http.StatusNotFound,
+		Code:       "ORGANIZATION-004",
+		Message:    "組織が見つかりません",
+	}
+	OrganizationInternalServerError = &APIError{
+		StatusCode: http.StatusInternalServerError,
+		Code:       "ORGANIZATION-005",
+		Message:    "組織の操作中にエラーが発生しました。時間をおいて再度お試しください",
+	}
 )
