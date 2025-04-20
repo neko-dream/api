d10c8409f2ae194d6a1d77ff1db7b8171a7e875f
haryoiro <mizusecocolte@gmail.com>
2025-04-20 10:51:21 +0900

    👍 一通りデプロイできるようになった

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,12 +13,18 @@ fi
 aws --profile $AWS_PROFILE ecr get-login-password --region ap-northeast-1 |
 docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com
 
+go mod tidy
+go mod download
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -tags timetzdata -ldflags="-s -w" -trimpath -o server ./cmd/server
+
 COMMIT_HASH=$(git show --format='%h' --no-patch)
 TIMESTAMP=$(date '+%s%3')
 export IMAGE_TAG=$COMMIT_HASH$TIMESTAMP
 TAG=$ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/kotohiro-prd-api:$IMAGE_TAG
 
-docker build -f ./container/Dockerfile . -t $TAG --no-cache
+docker build -f ./container/Dockerfile.gh . -t $TAG --no-cache
 docker push $TAG
+
+rm -rf ./server
 
 ecspresso deploy --config ./.ecspresso/api/prd/ecspresso.yml --force-new-deployment
