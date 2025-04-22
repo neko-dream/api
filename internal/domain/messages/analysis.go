2cca73521db2732307564732a8004e5f6957e295
haryoiro <mizusecocolte@gmail.com>
2025-04-22 19:47:08 +0900

    👕 fmt

--- internal/domain/messages/analysis.go
+++ internal/domain/messages/analysis.go
@@ -6,14 +6,14 @@ var (
 		Code:       "ANALYSIS-0000",
 		Message:    "レポートが見つかりません。",
 	}
-    GenerateAnalysisReportFailed = &APIError{
-        StatusCode: 500,
-        Code:       "ANALYSIS-0001",
-        Message:    "レポートの生成に失敗しました。",
-    }
-    AnalysisReportOpinionNotFound = &APIError{
-        StatusCode: 400,
-        Code:       "ANALYSIS-0002",
-        Message:    "意見がないためレポートを生成できません。",
-    }
+	GenerateAnalysisReportFailed = &APIError{
+		StatusCode: 500,
+		Code:       "ANALYSIS-0001",
+		Message:    "レポートの生成に失敗しました。",
+	}
+	AnalysisReportOpinionNotFound = &APIError{
+		StatusCode: 400,
+		Code:       "ANALYSIS-0002",
+		Message:    "意見がないためレポートを生成できません。",
+	}
 )
