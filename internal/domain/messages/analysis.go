592ecb604820c4530809141caa170d829ef82361
haryoiro <mizusecocolte@gmail.com>
2025-07-06 17:31:39 +0900

    👍 エラーメッセージを定義

--- internal/domain/messages/analysis.go
+++ internal/domain/messages/analysis.go
@@ -16,4 +16,15 @@ var (
 		Code:       "ANALYSIS-0002",
 		Message:    "意見がないためレポートを生成できません。",
 	}
+
+	AnalysisReportAlreadyFeedbacked = &APIError{
+		StatusCode: 400,
+		Code:       "ANALYSIS-0003",
+		Message:    "既にフィードバック済みのレポートです。",
+	}
+	AnalysisReportFeedbackFailed = &APIError{
+		StatusCode: 500,
+		Code:       "ANALYSIS-0004",
+		Message:    "レポートのフィードバックに失敗しました。",
+	}
 )
