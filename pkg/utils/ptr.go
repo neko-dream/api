fbcb868262738f58fccf2c14a552e45921dd39ac
haryoiro <mizusecocolte@gmail.com>
2025-09-15 14:44:45 +0900

    👍 ユーティリティ追加

--- pkg/utils/ptr.go
+++ pkg/utils/ptr.go
@@ -26,6 +26,41 @@ func ToPtrIfNotNullFunc[T any](nullFlag bool, getValue func() *T) *T {
 	return val
 }
 
+// ToPtrIf conditionがtrueの場合にポインタを返す
+// 変換元がsql.NullXXXのように、Validフィールドを持つような場合にの使用する。
+// 単純なポインタ変換には、lo.ToPtrを使用すること。
+// code:
+//
+//	// ok
+//	num := sql.NullInt32{Int32: 29, Valid: true}
+//	ptrNum := utils.ToPtrIf(num.Valid, num.Int32) // *int32 (29)
+//	num = sql.NullInt32{Valid: false}
+//	nilNum := utils.ToPtrIf(false, num.Int32) // nil
+//	// bad
+//	num := lo.ToPtr(10) // *int
+//	nilNum := utils.ToPtrIf(num != nil, *num) // panic
+func ToPtrIf[T any](condition bool, value T) *T {
+	if condition {
+		return &value
+	}
+	return nil
+}
+
+// MarshalToPtr conditionがtrueの場合に、valueをmarshalしてポインタを返す
+func OptionalMarshalToPtr[T encoding.TextMarshaler, U any](
+	isSet bool,
+	value T,
+	convert func(string) U,
+) *U {
+	if !isSet {
+		return nil
+	}
+	if bytes, err := value.MarshalText(); err == nil {
+		return lo.ToPtr(convert(string(bytes)))
+	}
+	return nil
+}
+
 // ToOpt ポインタ型をoas.OptXXX型に変換する
 // code:
 //
@@ -109,91 +144,94 @@ func ToOptNil[O any](v any) O {
 	}
 }
 
-// ToSQLNull ポインタ型をsql.NullXXX型に変換する
+// ToNullableSQL ポインタ型をsql.NullXXX型に変換する
 // code:
 //
 //	str := lo.ToPtr("example")
-//	nullStr := utils.ToSQLNull[sql.NullString](str) // sql.NullString{String: "example", Valid: true}
+//	nullStr := utils.ToNullableSQL[sql.NullString](str) // sql.NullString{String: "example", Valid: true}
 //	str = nil
-//	nullStr = utils.ToSQLNull[sql.NullString](str) // sql.NullString{Valid: false}
+//	nullStr = utils.ToNullableSQL[sql.NullString](str) // sql.NullString{Valid: false}
 //	num := 29
-//	nullNum := utils.ToSQLNull[sql.NullInt32](num) // sql.NullInt32{Int32: 29, Valid: true}
-func ToSQLNull[O any](v any) O {
-	switch val := v.(type) {
-	case *string:
-		if val == nil {
-			return any(sql.NullString{}).(O)
-		} else {
-			return any(sql.NullString{String: *val, Valid: true}).(O)
+//	nullNum := utils.ToNullableSQL[sql.NullInt32](num) // sql.NullInt32{Int32: 29, Valid: true}
+func ToNullableSQL[O any](v any) O {
+	rv := reflect.ValueOf(v)
+
+	if !rv.IsValid() {
+		return newNullValue[O]()
+	}
+
+	// ポインタのチェーンを辿ってnilチェック
+	for rv.Kind() == reflect.Ptr {
+		if rv.IsNil() {
+			return newNullValue[O]()
 		}
-	case string:
-		return any(sql.NullString{String: val, Valid: true}).(O)
-	case *int:
-		if val == nil {
-			return any(sql.NullInt32{}).(O)
-		} else {
-			return any(sql.NullInt32{Int32: int32(*val), Valid: true}).(O)
+		rv = rv.Elem()
+	}
+
+	if !rv.IsValid() {
+		return newNullValue[O]()
+	}
+
+	switch rv.Kind() {
+	case reflect.String:
+		return any(sql.NullString{String: rv.String(), Valid: true}).(O)
+	case reflect.Int8, reflect.Int16:
+		return any(sql.NullInt16{Int16: int16(rv.Int()), Valid: true}).(O)
+	case reflect.Int, reflect.Int32:
+		return any(sql.NullInt32{Int32: int32(rv.Int()), Valid: true}).(O)
+	case reflect.Int64:
+		return any(sql.NullInt64{Int64: rv.Int(), Valid: true}).(O)
+	case reflect.Uint8:
+		return any(sql.NullInt16{Int16: int16(rv.Uint()), Valid: true}).(O)
+	case reflect.Uint16:
+		return any(sql.NullInt32{Int32: int32(rv.Uint()), Valid: true}).(O)
+	case reflect.Uint, reflect.Uint32:
+		return any(sql.NullInt64{Int64: int64(rv.Uint()), Valid: true}).(O)
+	case reflect.Uint64:
+		if rv.Uint() > math.MaxInt64 {
+			return any(sql.NullInt64{Valid: false}).(O)
 		}
-	case int:
-		return any(sql.NullInt32{Int32: int32(val), Valid: true}).(O)
-	case *int32:
-		if val == nil {
-			return any(sql.NullInt32{}).(O)
-		} else {
-			return any(sql.NullInt32{Int32: *val, Valid: true}).(O)
-		}
-	case int32:
-		return any(sql.NullInt32{Int32: val, Valid: true}).(O)
-	case *int64:
-		if val == nil {
-			return any(sql.NullInt64{}).(O)
-		} else {
-			return any(sql.NullInt64{Int64: *val, Valid: true}).(O)
-		}
-	case int64:
-		return any(sql.NullInt64{Int64: val, Valid: true}).(O)
-	case *bool:
-		if val == nil {
-			return any(sql.NullBool{}).(O)
-		} else {
-			return any(sql.NullBool{Bool: *val, Valid: true}).(O)
-		}
-	case bool:
-		return any(sql.NullBool{Bool: val, Valid: true}).(O)
-	case *float32:
-		if val == nil {
-			return any(sql.NullFloat64{}).(O)
-		} else {
-			return any(sql.NullFloat64{Float64: float64(*val), Valid: true}).(O)
-		}
-	case float32:
-		return any(sql.NullFloat64{Float64: float64(val), Valid: true}).(O)
-	case *float64:
-		if val == nil {
-			return any(sql.NullFloat64{}).(O)
-		} else {
-			return any(sql.NullFloat64{Float64: *val, Valid: true}).(O)
-		}
-	case float64:
-		return any(sql.NullFloat64{Float64: val, Valid: true}).(O)
-	case *time.Time:
-		if val == nil {
-			return any(sql.NullTime{}).(O)
-		} else {
-			return any(sql.NullTime{Time: *val, Valid: true}).(O)
-		}
-	case time.Time:
-		return any(sql.NullTime{Time: val, Valid: true}).(O)
-	case *uuid.UUID:
-		if val == nil {
-			return any(uuid.NullUUID{}).(O)
-		} else {
-			return any(uuid.NullUUID{UUID: *val, Valid: true}).(O)
-		}
-	case uuid.UUID:
-		return any(uuid.NullUUID{UUID: val, Valid: true}).(O)
+		return any(sql.NullInt64{Int64: int64(rv.Uint()), Valid: true}).(O)
+	case reflect.Float32, reflect.Float64:
+		return any(sql.NullFloat64{Float64: rv.Float(), Valid: true}).(O)
+	case reflect.Bool:
+		return any(sql.NullBool{Bool: rv.Bool(), Valid: true}).(O)
 	default:
-		var zero O
+		val := rv.Interface()
+		switch v := val.(type) {
+		case time.Time:
+			return any(sql.NullTime{Time: v, Valid: true}).(O)
+		case uuid.UUID:
+			return any(uuid.NullUUID{UUID: v, Valid: true}).(O)
+		default:
+			return newNullValue[O]()
+		}
+	}
+}
+
+// newNullValue 指定されたsql.NullXXX型のゼロ値を返す
+func newNullValue[O any]() O {
+	var zero O
+	t := reflect.TypeOf(zero)
+
+	switch t {
+	case reflect.TypeOf(sql.NullString{}):
+		return any(sql.NullString{}).(O)
+	case reflect.TypeOf(sql.NullInt16{}):
+		return any(sql.NullInt16{}).(O)
+	case reflect.TypeOf(sql.NullInt32{}):
+		return any(sql.NullInt32{}).(O)
+	case reflect.TypeOf(sql.NullInt64{}):
+		return any(sql.NullInt64{}).(O)
+	case reflect.TypeOf(sql.NullBool{}):
+		return any(sql.NullBool{}).(O)
+	case reflect.TypeOf(sql.NullFloat64{}):
+		return any(sql.NullFloat64{}).(O)
+	case reflect.TypeOf(sql.NullTime{}):
+		return any(sql.NullTime{}).(O)
+	case reflect.TypeOf(uuid.NullUUID{}):
+		return any(uuid.NullUUID{}).(O)
+	default:
 		return zero
 	}
 }
