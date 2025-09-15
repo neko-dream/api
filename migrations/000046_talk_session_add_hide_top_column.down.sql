799e77be645520c82fcd04e20b0ae699b4f905c8
haryoiro <mizusecocolte@gmail.com>
2025-09-15 15:28:09 +0900

    🚀 index追加

--- migrations/000046_talk_session_add_hide_top_column.down.sql
+++ migrations/000046_talk_session_add_hide_top_column.down.sql
@@ -1,1 +1,3 @@
+DROP INDEX IF EXISTS idx_talk_sessions_hide_top;
+
 ALTER TABLE talk_sessions DROP COLUMN hide_top;
--- migrations/000046_talk_session_add_hide_top_column.up.sql
+++ migrations/000046_talk_session_add_hide_top_column.up.sql
@@ -1,1 +1,3 @@
 ALTER TABLE talk_sessions ADD COLUMN hide_top BOOLEAN NOT NULL DEFAULT FALSE;
+
+CREATE INDEX idx_talk_sessions_hide_top ON talk_sessions(hide_top) WHERE hide_top = FALSE;
